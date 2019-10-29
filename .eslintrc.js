5db1423ef25d58ce319bc74c503c7b6a1e2e7943
celine lemenn <celinelemenn@gmail.com>
2019-10-29 09:17:20 +0100

    set up

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "no-console": "off",
+    "comma-dangle": "off",
+    "quotes": "off",
+    "react/prop-types": 0,
+    "arrow-body-style": 0,
+    "space-before-function-paren": 0
+  },
+  "env": {
+    "browser": true
+  },
+  "parser": "babel-eslint"
+}
+
+
+
+
+
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es6": true
+//     },
+//     "extends": "airbnb",
+//     "parser": "babel-eslint"
+//     // "globals": {
+//     //     "Atomics": "readonly",
+//     //     "SharedArrayBuffer": "readonly"
+//     // },
+//     // "parserOptions": {
+//     //     "ecmaFeatures": {
+//     //         "jsx": true
+//     //     },
+//     //     "ecmaVersion": 2018,
+//     //     "sourceType": "module"
+//     // },
+//     // "plugins": [
+//     //     "react"
+//     // ],
+//     "rules": {
+//       "no-console": "off",
+//       "comma-dangle": "off",
+//       "quotes": "off",
+//       "react/prop-types": 0,
+//       "arrow-body-style": 0,
+//       "space-before-function-paren": 0
+//     }
+// };
